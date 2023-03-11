9c832939a378bfc8056adacfc82ccb9e9f56a291
Rehan Mondal <rehanmondal1312@gmail.com>
2023-03-10 23:56:50 -0800

    Read Dataset function fixed

--- functions/read_dataset.R
+++ functions/read_dataset.R
@@ -1,7 +1,10 @@
 read_dataset <- function(path){
-    dataset <- read.csv(path)
+    url <- "https://raw.githubusercontent.com/rehan13/DSCI-100-Project-Group25/master/tmdb_5000_movies.csv"
+    download.file(url, destfile = "data/Revenue.csv")
+    dataset <- read.csv("data/Revenue.csv")
     print(head(dataset))
 }
 
-read_dataset("data/tmdb_5000_movies.csv")
+read_dataset()
+
 

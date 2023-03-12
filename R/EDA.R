17b1ba102439de2299307df9ded0a9ed7f138944
Abheet Kansal <abheetkansal@gmail.com>
2023-03-12 12:28:52 -0700

    Added comments in 2 fuctions

--- R/EDA.R
+++ R/EDA.R
@@ -1,10 +1,32 @@
 library(dplyr)
 
+#' Read Dataset Function
+#'
+#' This function reads the dataset when a path is entered for the dataset and saves it at the destination entered
+#'
+#' @param path The path where the dataset is which needs to be stored in the given path
+#'
+#' @param dest The destination where the entered path for the dataset needs to be saved
+#' 
+#' @return Returns the dataset as a datframe
+#' 
+
 read_dataset <- function(path, dest){
     download.file(path, destfile = dest)
     dataset <- read.csv(dest)
     return(dataset)
 }
+
+#' Exploratory Data Analysis Function
+#'
+#' This function reads the dataset when a path is entered for the dataset and saves it at the destination entered
+#'
+#' @param path The path where the dataset is which needs to be stored in the given path
+#'
+#' @param dest The destination where the entered path for the dataset needs to be saved
+#' 
+#' @return Returns the dataset as a datframe
+#' 
 
 EDA <- function(data, select_cols, ...) {
     library(dplyr)
--- R/correlation_plot .R
+++ R/correlation_plot .R
@@ -1,6 +1,16 @@
 library(GGally)
 library(ggplot2)
 
+#' Correlation Plot Function
+#'
+#' This function gives a correlation plot by running ggpairs when a dataframe (data) is passed and a title for it
+#'
+#' @param data The dataframe that contains the dataset which needs to be plotted by ggpairs
+#'
+#' @param title The title that the correlation plot should contain passed as a string to the function
+#' 
+#' @return Returns the generated plot
+#' 
 correlation_plot <- function(data, title){
     ggpairs(data)+
         ggtitle(title)+

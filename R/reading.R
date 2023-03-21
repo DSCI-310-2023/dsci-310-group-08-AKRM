16b962a2f0c229cc0c4e68e6ea5a2b9e4078393f
Rehan Mondal <rehanmondal1312@gmail.com>
2023-03-20 19:57:31 -0700

    m3 remake

--- R/reading.R
+++
@@ -1,4 +0,0 @@
-source("R/eda.R")
-
-# download and read raw dataset
-data <- read_dataset("https://raw.githubusercontent.com/rehan13/Movie-Revenue-Predictor/main/tmdb_5000_movies.csv", "data/dataset.csv")
